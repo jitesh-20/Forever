c2af8793b091648fee586a12a2f3627eb30d2294
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-04-10 18:17:31 +0530

    first commit

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+                <div>
+                    <img src={assets.logo} className='mb-5 w-32' alt="Logo" />
+                    <p className='w-full md:w-2/3 text-gray-600'>
+                        Forever was born out of a passion for innovation and a desire to revolutionize the way people shop online. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the comfort of their homes.
+                    </p>
+                </div>
+
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>+1-212-456-7890</li>
+                        <li>contact@forever.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2024@forever.com - All Right Reserved.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
